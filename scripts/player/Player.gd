834f63ff492c8df3fda1488997b10bae0eae65cd
Andrey Naligatski Dias <andreydias@alunos.utfpr.edu.br>
2025-10-24 22:46:42 -0300

    added platform mechanic

--- scripts/player/Player.gd
+++ scripts/player/Player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
 @export var speed: float = 200.0
-@export var gravity: float = 1000.0
-@export var jump_force: float = -400.0
+@export var gravity: float = 1500.0
+@export var jump_force: float = -750.0
 @export var run_multiplier: float = 2.0
+@export var drop_through_duration: float = 0.2
+
+var dropping := false
 
 func get_input() -> Vector2:
 	var input_direction: Vector2 = Input.get_vector("player_left", "player_right", "player_up", "player_down")
@@ -23,5 +26,18 @@ func _physics_process(delta: float) -> void:
 	
 	if is_on_floor() and Input.is_action_just_pressed("player_jump"):
 		velocity.y = jump_force
+		
+	if Input.is_action_pressed("player_down"):
+		drop_through_platform()
 
 	move_and_slide()
+	
+func drop_through_platform():
+	if dropping:
+		return
+		
+	dropping = true
+	set_collision_mask_value(1, false)
+	await get_tree().create_timer(drop_through_duration).timeout
+	set_collision_mask_value(1, true)
+	dropping = false
