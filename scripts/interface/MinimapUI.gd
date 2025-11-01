09e572967c7164cbb5b35d6d69f503a6cd8a5651
Andrey Naligatski Dias <andreydias@alunos.utfpr.edu.br>
2025-11-01 00:31:28 -0300

    minimap camera now follows the player

--- scripts/interface/MinimapUI.gd
+++ scripts/interface/MinimapUI.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @onready var minimap_viewport: SubViewport = $MinimapContainer/MinimapSubview
-@onready var minimap_camera: Camera2D = $MinimapContainer/MinimapSubview/Camera2D
+@onready var minimap_camera: Camera2D = $MinimapContainer/MinimapSubview/MinimapCamera
 
 var player: Node2D
 
@@ -11,11 +11,14 @@ var player: Node2D
 func _ready() -> void:
 	minimap_viewport.world_2d = get_tree().root.world_2d
 	
+	await get_tree().process_frame
+
 	player = get_tree().get_first_node_in_group("player")
+	
 	if not player:
 		var parent = get_tree().current_scene
 		player = parent.get_node_or_null("Player")
 
 func _physics_process(delta: float) -> void:
 	if player and minimap_camera:
-		minimap_camera.global_position = player.global_position
+		minimap_camera.position = player.position
